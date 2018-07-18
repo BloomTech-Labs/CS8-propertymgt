2b54a684a0e0e21c5cf2f4e4f7e62fddf4a7db0a
BonnW <pwonghansa@gmail.com>
2018-07-18 17:02:52 -0500

    Rendered Carousel. Added Landing to root App.js

--- src/lspmgt_frontend/src/App.js
+++ src/lspmgt_frontend/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./semantic/dist/semantic.min.css";
 import "./App.css";
 import { Container } from "semantic-ui-react";
-import LoginForm from "./components/LandingPage/LoginForm";
+import LoginForm from "./Components/LandingPage/LoginForm";
+import Landing from "./Components/LandingPage/Landing";
 
 class App extends Component {
   render() {
     return (
       <Container>
+        <Landing />;
         <LoginForm />;
       </Container>
     );
