cf51cd6999cac3026643e030334bdbdab3026131
BonnW <pwonghansa@gmail.com>
2018-07-18 23:01:31 -0500

    added button to landing

--- src/lspmgt_frontend/src/Components/LandingPage/Landing.js
+++ src/lspmgt_frontend/src/Components/LandingPage/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import LandingCarousel from "./LandingCarousel";
+import { Button } from "semantic-ui-react";
 
 export default class Landing extends Component {
   constructor(props) {
@@ -14,6 +15,10 @@ export default class Landing extends Component {
     return (
       <div>
         <LandingCarousel />
+        <div className="landingButtons">
+          <Button primary>Buy Now</Button>
+          <Button secondary>Sign in</Button>
+        </div>
       </div>
     );
   }
