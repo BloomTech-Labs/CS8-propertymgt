082a41c4096f9e5c99d09c857e2f1de46aa17011
BonnW <pwonghansa@gmail.com>
2018-07-20 14:14:11 -0500

    post (kinda) working, error: missing propertyId. need to consult team

--- src/Users/userRouter.js
+++ src/Users/userRouter.js
@@ -3,24 +3,46 @@
 const express = require('express');
 const router = express.Router();
 const dd = require('../Config/AwsConfig');
-const User = require('./userModels');
+
+// global TableName in params is 'ls_property_mgt'
 
 router.get('/', (req, res) => {
   const params = {
     TableName: 'ls_property_mgt',
-    // using a temporary key, does not work yet
-    Key: {
-      userId: 0,
-    },
+    // temporary key, does not work yet
+    // Key: {
+    //   userId: 0,
+    // },
   };
 
-  dd.get(params, (err, data) => {
+  dd.scan(params, (err, data) => {
     if (err) {
       res.status(200).json({ status: 'error', error: err });
     }
     res.status(200).json({ status: 'success', data: data });
   });
   // res.status(200).json({ status: 'userRouter connected properly' });
+});
+
+router.post('/signup', (req, res) => {
+  const params = {
+    TableName: 'ls_property_mgt',
+    Item: {
+      Name: req.Name,
+      Email: req.Email,
+      Phone: req.phoneNumber,
+    },
+  };
+
+  dd.put(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(200).json({ status: 'error', error: err });
+    } else {
+      console.log(data);
+      res.status(200).json({ status: 'success', data: data });
+    }
+  });
 });
 
 module.exports = router;
