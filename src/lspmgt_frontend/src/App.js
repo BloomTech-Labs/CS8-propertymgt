a0ce6b6cbcbff56859fd906b676c1bc3ede944b5
codercodingthecode <andersonlavor@gmail.com>
2018-07-20 17:18:36 -0400

    COGNITO SETUP - Added cognito and aws amplify libs - Wrote a sample sign up snippet

--- src/lspmgt_frontend/src/App.js
+++ src/lspmgt_frontend/src/App.js
@@ -7,6 +7,11 @@ import LoginForm from './Components/LandingPage/LoginForm';
 import Landing from './Components/LandingPage/Landing';
 import AdminNavigation from './Components/Admin/AdminNavigation';
 
+// this is a test for aws cognito auth.
+import Amplify, { Auth } from 'aws-amplify';
+import AmplifyConfig from './Config/Auth';
+Amplify.configure(AmplifyConfig);
+
 const LandingPage = () => {
   return (
     <Container>
@@ -16,8 +21,23 @@ const LandingPage = () => {
   );
 };
 
+const LoginTest = () => {
+  Auth.signUp({
+    username: 'lambdapropertymgt@gmail.com',
+    password: '*.abcABC12345',
+  })
+    .then(data => {
+      console.log('success, --> ', data);
+    })
+    .catch(err => {
+      console.log('there was an error -> ', err);
+    });
+};
+
 class App extends Component {
   render() {
+    console.log('This is amplify config -> ', AmplifyConfig);
+    // LoginTest();
     return (
       <div>
         <Route exact path="/" component={LandingPage} />
