8bf17af2143f2e8a10819e86bc83e6242637054a
BonnW <pwonghansa@gmail.com>
2018-07-23 05:49:07 -0500

    cleanup commit, cleaned up all warnings in working files

--- src/lspmgt_frontend/src/Components/Payment/BuyNowModal.js
+++ src/lspmgt_frontend/src/Components/Payment/BuyNowModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import { Button, Header, Modal } from 'semantic-ui-react'
 
 
 class BuyNowModal extends Component {
