fc9a1591f74e9896d834120bddbc11dafcf5ec20
BonnW <pwonghansa@gmail.com>
2018-07-23 01:23:28 -0500

    first test charge successful, ahem, FIRST TRYYYYYYYY. erhm erhm, secret key hidden property

--- src/Billing/BillingRouter.js
+++ src/Billing/BillingRouter.js
@@ -3,8 +3,28 @@ const router = express.Router();
 // const dd = require('../Config/AwsConfig');
 // const dbModel = require('../Config/Dbmodel');
 
+const stripeKey = require('./StripeSK');
+
+// second attribute passed is the secret key 
+const stripe = require('stripe')(stripeKey.SK);
+
+
 router.get('/', (req, res) => {
     res.status(200).json({ status: 'billing route connected' });
 });
+
+// Stripe Charge Route
+router.post('/charge', (req, res) => {
+    const cost = 2500;
+    stripe.charges.create({
+        amount: cost,
+        currency: 'cad',
+        source: 'tok_amex',
+        description: 'Hello Stripe World'
+    }, function (err, charge) {
+        if (err) res.status(200).json({ status: 'error', error: err });
+        res.status(200).json({ status: 'charge complete', data: charge });
+    })
+})
 
 module.exports = router;
