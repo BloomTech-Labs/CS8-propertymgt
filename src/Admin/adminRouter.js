819e373bfb72cd1557f63bfc6a1abe1cc218b901
codercodingthecode <andersonlavor@gmail.com>
2018-07-24 17:53:19 -0400

    DYNAMODB RE MAP - Created DynamoDbTables to hold an object mapping for DynamoDB tables - Prettier changed other files

--- src/Admin/adminRouter.js
+++ src/Admin/adminRouter.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const dd = require("../Config/AwsConfig");
-const dbModel = require("../Config/DbModel");
-const faker = require("faker");
+const dd = require('../Config/AwsConfig');
+const dbModel = require('../Config/DbModel');
+
+// used for development only
+const faker = require('faker');
 
 const GlobalParams = {
-  TableName: "ls_property_mgt"
+  TableName: 'ls_property_mgt',
 };
 
 let PROPERTY_ID = 1;
@@ -15,39 +17,39 @@ const IncrementId = () => {
 };
 
 // return all the properties for property cards screen
-router.get("/properties", (req, res) => {
+router.get('/properties', (req, res) => {
   dd.scan(GlobalParams, (err, data) => {
     if (err) {
-      res.status(200).json({ status: "error", error: err });
-    } else res.status(200).json({ status: "success", data: data });
+      res.status(200).json({ status: 'error', error: err });
+    } else res.status(200).json({ status: 'success', data: data });
   });
 });
 
 // return a single property using the id parameter
-router.get("/property/:id", (req, res) => {
+router.get('/property/:id', (req, res) => {
   const params = {
-    TableName: "ls_property_mgt",
+    TableName: 'ls_property_mgt',
     Key: {
-      propertyId: Number(req.params.id)
-    }
+      propertyId: Number(req.params.id),
+    },
   };
 
   dd.get(params, (err, data) => {
     if (err) {
-      res.status(200).json({ status: "error", error: err });
+      res.status(200).json({ status: 'error', error: err });
     } else {
       console.log(typeof params.Key.propertyId);
-      res.status(200).json({ status: "success", data: data });
+      res.status(200).json({ status: 'success', data: data });
     }
   });
 });
 
 // **post, add a new property to database
-router.post("/addproperty", (req, res) => {
+router.post('/addproperty', (req, res) => {
   console.log(req);
 
   const params = {
-    TableName: "ls_property_mgt",
+    TableName: 'ls_property_mgt',
     Item: {
       NameOwner: req.body.NameOwner,
       EmailOwner: req.body.EmailOwner,
@@ -59,60 +61,60 @@ router.post("/addproperty", (req, res) => {
       Bedrooms: req.body.Bedrooms,
       Bathrooms: req.body.Bathrooms,
       YearBuilt: req.body.YearBuilt,
-      propertyId: IncrementId()
-    }
+      propertyId: IncrementId(),
+    },
   };
 
-  console.log("params in server ", params);
+  console.log('params in server ', params);
 
   dd.put(params, (err, data) => {
-    if (err) console.log("Error trying to POST", err);
+    if (err) console.log('Error trying to POST', err);
   });
-  res.status(200).json({ status: "added property" });
+  res.status(200).json({ status: 'added property' });
 });
 
 // deletes a single property with the given property id
-router.delete("/property", (req, res) => {
+router.delete('/property', (req, res) => {
   const params = {
-    TableName: "ls_property_mgt",
+    TableName: 'ls_property_mgt',
     Key: {
-      propertyId: Number(req.params.id)
-    }
+      propertyId: Number(req.params.id),
+    },
   };
 
   dd.delete(params, (err, data) => {
     if (err) console.log(err);
     else console.log(data);
-    res.status(200).json({ status: "deleted property" });
+    res.status(200).json({ status: 'deleted property' });
   });
 });
 
 // return all the work orders for cards screen
-router.get("/workorder", (req, res) => {
+router.get('/workorder', (req, res) => {
   dd.scan(GlobalParams, (err, data) => {
-    if (err) res.status(200).json({ status: "error", error: err });
-    else res.status(200).json({ status: "success", data: data });
+    if (err) res.status(200).json({ status: 'error', error: err });
+    else res.status(200).json({ status: 'success', data: data });
   });
 });
 
 // **post, it adds a new tenant to the system
-router.get("/addtenant", (req, res) => {
-  res.status(200).json({ status: "add a new tenant to the system" });
+router.get('/addtenant', (req, res) => {
+  res.status(200).json({ status: 'add a new tenant to the system' });
 });
 
 // display the billing information
-router.get("/billing", (req, res) => {
-  res.status(200).json({ status: "returns billing information" });
+router.get('/billing', (req, res) => {
+  res.status(200).json({ status: 'returns billing information' });
 });
 
 // display admin settings
-router.get("/settings", (req, res) => {
-  res.status(200).json({ status: "display admin settings info" });
+router.get('/settings', (req, res) => {
+  res.status(200).json({ status: 'display admin settings info' });
 });
 
 // **put, update setting in admin settings
-router.get("/settingsupdate", (req, res) => {
-  res.status(200).json({ status: "returns all properties for cards" });
+router.get('/settingsupdate', (req, res) => {
+  res.status(200).json({ status: 'returns all properties for cards' });
 });
 
 module.exports = router;
---
+++ src/Config/DynamoDbTables.js
@@ -0,0 +1,11 @@
+// Tables in DynamoDB
+// LS_DB -> Database possibly contains Admin, Tenant and Property
+// Admins -> For Admin data
+// Properties -> For Properties
+// Tenants -> Tenant info
+module.exports = tables = {
+  LS_DB,
+  Admins,
+  Properties,
+  Tenants,
+};
