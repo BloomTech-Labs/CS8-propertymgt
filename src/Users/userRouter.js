f9e68e20564d31e903d30eb1c4cc364daf65f6a8
Shaun <shaun37k@gmail.com>
2018-07-24 16:37:49 -0400

    destructuring, double quotes to single, use property shorthand (d=data)

--- src/Users/userRouter.js
+++ src/Users/userRouter.js
@@ -1,8 +1,9 @@
 // Option A is that we can make User it's own model and route
 
 const express = require('express');
-const router = express.Router();
 const dd = require('../Config/AwsConfig');
+
+const router = express.Router();
 
 // global TableName in params is 'ls_property_mgt'
 
@@ -15,11 +16,11 @@ router.get('/', (req, res) => {
     // },
   };
 
-  dd.scan(params, (err, data) => {
+  dd.scan(params, (err, d) => {
     if (err) {
       res.status(200).json({ status: 'error', error: err });
     }
-    res.status(200).json({ status: 'success', data: data });
+    res.status(200).json({ status: 'success', data: d });
   });
   // res.status(200).json({ status: 'userRouter connected properly' });
 });
@@ -39,8 +40,8 @@ router.post('/signup', (req, res) => {
       console.log(err);
       res.status(200).json({ status: 'error', error: err });
     } else {
-      console.log(data);
-      res.status(200).json({ status: 'success', data: data });
+      console.log(d);
+      res.status(200).json({ status: 'success', data: data;
     }
   });
 });
