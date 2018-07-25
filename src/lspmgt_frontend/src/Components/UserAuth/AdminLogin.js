261892385e677a24b9687d933fcfa03646264a82
BonnW <pwonghansa@gmail.com>
2018-07-25 18:05:32 -0500

    Redone Login route. Now shows login for Admin and Tenant instead of sign up

---
+++ src/lspmgt_frontend/src/Components/UserAuth/AdminLogin.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Form, Button, Modal, Header, Segment } from 'semantic-ui-react';
+import axios from 'axios';
+
+class AdminLogin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Email: '',
+      Password: '',
+    };
+  }
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    // eslint suggests destructuring the next line
+    const { username, password } = this.state;
+    if (username && password) {
+      axios
+        // need /login route via userRouter.js, nonexistent
+        .post('/login', this.state)
+        .then()
+        .catch();
+    }
+  };
+
+  canBeDisplayed = () => {
+    const { Email, Password } = this.state;
+    return Email.length > 0 && Password.length > 0;
+  };
+
+  render() {
+    return (
+      <Modal trigger={<Button primary>Admin Login</Button>}>
+        <Modal.Header>Admin Login</Modal.Header>
+        <Modal.Content>
+          <Form size="large" style={{ maxWidth: '100%' }}>
+            <Segment raised style={{ maxWidth: '100%' }}>
+              <Form.Input fluid icon="user" iconPosition="left" placeholder="E-mail address" />
+              <Form.Input fluid icon="lock" iconPosition="left" placeholder="Password" />
+              <Button color="blue" fluid size="large">
+                Login
+              </Button>
+            </Segment>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    );
+  }
+}
+
+export default AdminLogin;
---
+++ src/lspmgt_frontend/src/Components/UserAuth/TenantLogin.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Form, Button, Modal, Header, Segment } from 'semantic-ui-react';
+import axios from 'axios';
+
+class TenantLogin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Email: '',
+      Password: '',
+    };
+  }
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    // eslint suggests destructuring the next line
+    const { username, password } = this.state;
+    if (username && password) {
+      axios
+        // need /login route via userRouter.js, nonexistent
+        .post('/login', this.state)
+        .then()
+        .catch();
+    }
+  };
+
+  canBeDisplayed = () => {
+    const { Name, Email, Phone } = this.state;
+    return Name.length > 0 && Email.length > 0 && Phone.length > 0;
+  };
+
+  render() {
+    return (
+      <Modal trigger={<Button primary>Tenant Login</Button>}>
+        <Modal.Header>Tenant Login</Modal.Header>
+        <Modal.Content>
+          <Form size="large" style={{ maxWidth: '100%' }}>
+            <Segment raised style={{ maxWidth: '100%' }}>
+              <Form.Input fluid icon="user" iconPosition="left" placeholder="E-mail address" />
+              <Form.Input fluid icon="lock" iconPosition="left" placeholder="Password" />
+              <Button color="blue" fluid size="large">
+                Login
+              </Button>
+            </Segment>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    );
+  }
+}
+
+export default TenantLogin;
