b2006b7b42482f063d30043ee92c0053e2804970
BonnW <pwonghansa@gmail.com>
2018-07-25 20:12:02 -0500

    payments route base code written. labled what needs to be added

--- src/lspmgt_frontend/src/Components/Tenant/Payments/Payments.js
+++ src/lspmgt_frontend/src/Components/Tenant/Payments/Payments.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Grid, Segment, Form } from 'semantic-ui-react';
 
-const Payments = () => {
-  return <div>Hello Payments</div>;
-};
+class Payments extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Grid divided="vertically">
+        <Grid.Row>
+          <Segment basic>Outstanding Balance</Segment>
+        </Grid.Row>
+        <Grid.Row column={3}>
+          <Grid.Column>
+            <Segment basic>Payment Details</Segment>
+            <Segment basic>Payment Method</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment basic>Payment info Card or Bank account</Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+}
 
 export default Payments;
