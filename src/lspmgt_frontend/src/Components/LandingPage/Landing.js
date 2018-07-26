013001d4e0384a75d1d604061d9284172acbb6e6
codercodingthecode <andersonlavor@gmail.com>
2018-07-26 12:22:42 -0400

    Test for semantinc ui

--- src/lspmgt_frontend/src/Components/LandingPage/Landing.js
+++ src/lspmgt_frontend/src/Components/LandingPage/Landing.js
@@ -42,6 +42,8 @@ export default class Landing extends Component {
             dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
             mollit anim id est laborum.
+
+            Testing semantic ui
           </p>
         </div>
       </div>
