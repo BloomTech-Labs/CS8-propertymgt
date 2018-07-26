d28adf528792b09d4cee9b574ca62d2ec1164082
BonnW <pwonghansa@gmail.com>
2018-07-25 19:14:43 -0500

    dashboard base design added

--- src/lspmgt_frontend/src/Components/Tenant/Dashboard/Dashboard.js
+++ src/lspmgt_frontend/src/Components/Tenant/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Segment, Divider } from 'semantic-ui-react';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -9,9 +10,20 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div>
-        <p>dashboard</p>
-      </div>
+      <Grid columns={2} relaxed style={{ maxWidth: '100%' }}>
+        <Grid.Column>
+          <Segment basic>Outstanding Balance</Segment>
+          <Segment basic>Make Payment</Segment>
+          <Segment basic>Submit a work order</Segment>
+          <Segment basic>Alerts</Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment basic>Office Address</Segment>
+          <Segment basic>Office Number</Segment>
+          <Segment basic>landlords email</Segment>
+          <Segment basic>Maintenance info</Segment>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
