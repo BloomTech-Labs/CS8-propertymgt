d442c74c3c8dc7ba72023a3d85c12204e0a97c88
BonnW <pwonghansa@gmail.com>
2018-07-25 20:24:55 -0500

    added base code for settings.js and workorders.js. now displays data and indicates what should be there

--- src/lspmgt_frontend/src/Components/Tenant/Settings/Settings.js
+++ src/lspmgt_frontend/src/Components/Tenant/Settings/Settings.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Grid, Segment, Form } from 'semantic-ui-react';
 
-const Settings = () => {
-  return <div>Hello Settings</div>;
-};
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Grid.Column>
+          <Segment basic>Email</Segment>
+          <Segment basic>Mobile#</Segment>
+          <Segment basic>Get Text or Emails?</Segment>
+          <Segment basic>Old Password</Segment>
+          <Segment basic>New Password</Segment>
+          <Segment basic>Save Changes</Segment>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
 
 export default Settings;
--- src/lspmgt_frontend/src/Components/Tenant/WorkOrders/WorkOrders.js
+++ src/lspmgt_frontend/src/Components/Tenant/WorkOrders/WorkOrders.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Grid, Segment, Form } from 'semantic-ui-react';
 
-const WorkOrders = () => {
-  return <div>Hello WorkOrders</div>;
-};
+class WorkOrders extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Grid divided="vertically">
+        <Grid.Row>
+          <Segment basic>Submit a Work Order</Segment>
+        </Grid.Row>
+        <Grid.Row column={2}>
+          <Grid.Column>
+            <Segment basic>Address</Segment>
+            <Segment basic>Description of issue</Segment>
+            <Segment basic>Phone Number</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment basic>Related Photo</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Segment basic>Permission to enter</Segment>
+          <Segment basic>Submit form</Segment>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+}
 
 export default WorkOrders;
