f6e6813136adf7b2616ec2396a0eb2526ccde1e8
codercodingthecode <andersonlavor@gmail.com>
2018-07-26 20:05:45 -0400

    FIXED BACKEND API - Backend apis now works and also renders the website _ Deleted netlify.toml file, not needed anymore - Created Home folder and Route to serve the website

---
+++ src/Home/Home.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'src/lspmgt_frontend/build', 'index.html'));
+});
+
+module.exports = router;
