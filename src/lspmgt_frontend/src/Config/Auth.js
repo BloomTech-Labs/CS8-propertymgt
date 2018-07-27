60b8b540d2e6ca85f150db2c4943b8e871b72336
codercodingthecode <andersonlavor@gmail.com>
2018-07-27 01:07:34 -0400

    RE CONFIGURED ENV VARIABLES - Re set env var on Heroku and made changes to react to accurately reflect system env vars

--- src/lspmgt_frontend/src/Config/Auth.js
+++ src/lspmgt_frontend/src/Config/Auth.js
@@ -1,13 +1,13 @@
 const AmplifyConfig = {
   Auth: {
     // REQUIRED - Amazon Cognito Identity Pool ID
-    identityPoolId: process.env.IDPID, 
+    identityPoolId: process.env.REACT_APP_IDPID, 
     // REQUIRED - Amazon Cognito Region
-    region: process.env.REGION_AUTH,
+    region: process.env.REACT_APP_REGION,
     // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolId: process.env.UPID,
+    userPoolId: process.env.REACT_APP_UPID,
     // OPTIONAL - Amazon Cognito Web Client ID
-    userPoolWebClientId: process.env.UPWCID,
+    userPoolWebClientId: process.env.REACT_APP_UPWCID,
   },
 };
 
