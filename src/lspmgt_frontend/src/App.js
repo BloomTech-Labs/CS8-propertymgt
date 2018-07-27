22117caa344f2d7d3773ceca8f3062e9754f9329
Erik Alonso <mr.alonsoea@protonmail.ch>
2018-07-26 19:13:08 -0500

    restructured admin folders for intuitive navigation
    - edited folder paths for each component

--- src/lspmgt_frontend/src/App.js
+++ src/lspmgt_frontend/src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-// import './semantic/dist/semantic.min.css';
 import './App.css';
 import { Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import Login from './Components/UserAuth/Login';
-// import Landing from './Components/LandingPage/Landing';
 import LandingProto from './Components/LandingPage/LandingPrototype';
-
-import AdminNavigation from './Components/Admin/AdminNavigation';
+import AdminNavigation from './Components/Admin/Navigation/AdminNavigation';
 import TenantNavigation from './Components/Tenant/Navigation/TenantNavigation';
-// import SignUpForm from './Components/UserAuth/SignUpForm';
 
 // this is a test for aws cognito auth.
 // import Amplify, { Auth } from 'aws-amplify';
@@ -20,7 +16,6 @@ const LandingPage = () => {
   return (
     <Container>
       <LandingProto />
-      {/* <LoginForm /> */}
     </Container>
   );
 };
@@ -31,10 +26,8 @@ class App extends Component {
       <div>
         <Route exact path="/" component={LandingPage} />
         <Route path="/login" component={Login} />
-        {/* <Route path="/signup" component={SignUpForm} /> */}
         <Route path="/admin/(.+)" component={AdminNavigation} />
         <Route path="/tenant/(.+)" component={TenantNavigation} />
-        {/* <Route path="/payment" component={Payment} /> */}
       </div>
     );
   }
