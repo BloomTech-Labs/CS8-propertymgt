6f46326a1ea61cfdf9f4e41893246b751347562f
BonnW <pwonghansa@gmail.com>
2018-08-01 19:08:41 -0500

    fixed bug from previous commit

--- src/lspmgt_frontend/src/Components/Common/NavBar.js
+++ src/lspmgt_frontend/src/Components/Common/NavBar.js
@@ -7,24 +7,21 @@ export default class NavBar extends Component {
       <Menu stackable fluid inverted>
         <Menu.Item>LS PROPERTY MANAGEMENT</Menu.Item>
 
-          <Menu.Menu position="right">
-            <Link to="/login">
-              <Menu.Item>Signup</Menu.Item>
-            </Link>
-            <Link to="/login">
-              <Menu.Item>Signin</Menu.Item>
-            </Link>
-            <Link to="/login">
-              <Menu.Item>Sign out</Menu.Item>
-            </Link>
-            {/* <Menu.Item
+        <Menu.Menu position="right">
+          <Link to="/login">
+            <Menu.Item>Signup</Menu.Item>
+          </Link>
+          <Link to="/login">
+            <Menu.Item>Signin</Menu.Item>
+          </Link>
+
+          {/* <Menu.Item
                     name='sign-in'
                     active={activeItem === 'sign-in'}
                     onClick={this.handleItemClick}
                 >
                     Sign-in
                 </Menu.Item> */}
-          </Menu.Menu>
         </Menu.Menu>
       </Menu>
     );
