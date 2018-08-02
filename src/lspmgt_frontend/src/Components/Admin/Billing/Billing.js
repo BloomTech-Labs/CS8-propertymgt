6fe20bf61ebee6f33b2bdb6fb7768ab1f8c239f6
BonnW <pwonghansa@gmail.com>
2018-08-02 18:45:36 -0500

    changed billing page for admin to recieve list of properties and reflect that through the page

--- src/lspmgt_frontend/src/Components/Admin/Billing/Billing.js
+++ src/lspmgt_frontend/src/Components/Admin/Billing/Billing.js
@@ -1,37 +1,81 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Dropdown } from 'semantic-ui-react';
 import BillingForm from './BillingForm';
 import Selector from './PropertySelector';
+import axios from 'axios';
 
 class Billing extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      currentProperty: 1,
+      SelectedProperty: '',
+      PropertyList: [],
     };
 
-    this.changeProperty = this.changeProperty.bind(this);
+    this.setProperty = this.setProperty.bind(this);
   }
 
-  changeProperty(newProperty) {
-    this.setState({
-      currentProperty: newProperty,
-    });
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/property/all')
+      .then((res) => {
+        this.setState({
+          PropertyList: res.data.data.Items,
+        });
+      })
+      .catch((error) => {
+        console.log('Error in Billing GET');
+      });
   }
+
+  getPropList = () => {
+    const myArr = this.state.PropertyList.map((property, index) => {
+      const constructingTheObject = {
+        key: index,
+        value: property.PropertyAddr,
+        flag: '',
+      };
+      const array = Object.keys(property);
+      const x = array.indexOf('PropertyAddr');
+      const addrToText = property[Object.keys(property)[x]];
+      const propertyWithText = Object.assign({ text: addrToText }, constructingTheObject);
+      return propertyWithText;
+    });
+    return myArr;
+  };
+
+  setProperty = (e, { name, value }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
 
   render() {
-    const { currentProperty } = this.state;
+    const { SelectedProperty } = this.state;
+
+    const testArr = this.getPropList();
 
     return (
       <div>
         <Grid celled>
+          <Grid.Row textAlign="left">
+            <Dropdown
+              placeholder="Select Property"
+              selection
+              name="SelectedProperty"
+              options={testArr}
+              onChange={this.setProperty}
+            />
+          </Grid.Row>
           <Grid.Row>
             <Grid.Column width={3}>
-              <Selector onChange={this.changeProperty} />
+              <p>Saved Card Info of {SelectedProperty}</p>
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <p>Rent History of {SelectedProperty}</p>
             </Grid.Column>
           </Grid.Row>
-          <BillingForm property={currentProperty} />
         </Grid>
       </div>
     );
